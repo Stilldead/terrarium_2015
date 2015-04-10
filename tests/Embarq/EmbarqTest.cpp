d0a40533052267fadc44d13455d34eb85f527052
PruDyl <p13006720@MaquetteIutInfo.salsa.univ-amu.fr>
2015-04-10 16:27:35 +0200

    ADD Tests xBee

--- tests/Embarq/EmbarqTest.cpp
+++ tests/Embarq/EmbarqTest.cpp
@@ -20,3 +20,29 @@ TEST(Embarq, Test)
 {
   FAIL("Fail me");
 }
+
+TEST(Embarq, Test_returnTab)
+{
+  string entete = "32";
+  string expediteur = "11.111.111.11";
+  string destinataire = "22.222.222.22";
+  string message = "coucou";
+  string queue = "fin";
+  string tab[] = {entete, expediteur, destinataire, message, queue};
+  bytes datagramme[5] = {entete.getBytes(), expediteur.getBytes(), destinataire.getBytes(), message.getBytes(), queue.getBytes()};
+ 
+  CHECK_EQUALS(tab, receiveData(datagramme));
+}
+
+TEST(Embarq, Test_returnTab)
+{
+  string entete = "64";
+  string expediteur = "11.111.111.11";
+  string destinataire = "22.222.222.22";
+  string message = "salut";
+  string queue = "fin";
+  string tab[] = {entete, expediteur, destinataire, message, queue};
+  bytes datagramme[5] = {entete.getBytes(), expediteur.getBytes(), destinataire.getBytes(), message.getBytes(), queue.getBytes()};
+ 
+  CHECK_EQUALS(tab, receiveData(datagramme));
+}
